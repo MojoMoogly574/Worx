4b0a46056f0b887a020fb176cda4a0edf85c3f2c
Joseph DeWeese <jdeweese@weldtascics.systems>
2023-10-13 15:36:25 -0500

    Initial Commit

---
+++ Worx/WorxApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorxApp.swift
+//  Worx
+//
+//  Created by J. DeWeese on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct WorxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
